fe17e0f7349b7fdc485d285a10d94e38faa1c86c
bomilosavljevic <bomilosavljevic@CTSI-BORISM>
2021-01-28 17:03:55 +0100

    End Of Section 2

---
+++ API/Controllers/ContactsController.cs
@@ -0,0 +1,38 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.Entity.Infrastructure;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ContactsController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ContactsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Contact>> GetUsers()
+        {
+            return _context.Contacts.ToList();
+        }
+
+        // api/Contacts/3
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Contact>> GetUser(int id)
+        {
+            return await _context.Contacts.FindAsync(id);
+        }
+        
+    }
+}
